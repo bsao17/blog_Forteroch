813ed997cfc724ffa65107eae7a43580599bebf2
bsao17 <greta.b.meddhed>
2020-10-27 21:25:47 +0100

    display data

--- views/ACCOUNT/accountBillets.php
+++ views/ACCOUNT/accountBillets.php
@@ -10,17 +10,22 @@
 </a>
 
 <section class="m-2 p-2 border border-light rounded" id="billet">
+    <h1 id="title"></h1>
+    <p id="articles" class="card"></p>
     
 </section>
 
 <script>
     const url = "?action=apidata/billets";
     let billetData = [];
+    let articles = document.getElementById('articles')
+    let title = document.getElementById('title')
     fetch(url).then(response=>response.json()).then(billets=>{
       
-        for(billet of billets){
-            billetData.push(billet[2]);
+        for( i = 0; i < billets.length; i++){
+            billetData.push(billets[i]);
             console.log(billetData);
+            articles.innerHTML = billetData[i].title
         }
     });
 </script>
