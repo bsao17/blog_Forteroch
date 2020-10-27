3989b9db81d825f1c6f95e983f9552389d132faf
bsao17 <greta.b.meddhed>
2020-10-27 18:53:38 +0100

    Add request ajax to my API

--- views/ACCOUNT/accountBillets.php
+++ views/ACCOUNT/accountBillets.php
@@ -10,14 +10,20 @@
 </a>
 
 <section class="m-2 p-2 border border-light rounded" id="billet">
-    <?php
-        foreach($post as $value){
-            echo "<strong class='text-center text-light'>".$value['title']."</strong>";
-            echo "<span class='card p-3' id='{$value['ID']}'>".$value['content']."<a href='#' class='btn btn-outline-light m-auto w-25'><span class='text-muted h4'><i class='fas fa-hand-point-up'></span></i></a></span><hr class='border border-light bg-danger p-2 rounded-pill'>";
-        }
-        
-    ?>
+    
 </section>
+
+<script>
+    const url = "?action=apidata/billets";
+    let billetData = [];
+    fetch(url).then(response=>response.json()).then(billets=>{
+      
+        for(billet of billets){
+            billetData.push(billet[2]);
+            console.log(billetData);
+        }
+    });
+</script>
 
 <?php  
     require_once('./views/commentFormView.php');
